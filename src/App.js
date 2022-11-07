38d5ab5dc36d447b92490a53be8bab7e34c02667
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:46:16 -0500

    Add new text and gator image
    Updated name in App.js
    
    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Darian and this is my project </p>
       </header>
     </div>
   );
