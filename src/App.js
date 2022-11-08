b5d6bf5e6ff7b846b0fcfcea8004f518b18ace79
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    added new project text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Jon and this is my project </p>
+        <p>
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
