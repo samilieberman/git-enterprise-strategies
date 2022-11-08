6de629efcf36f8228b800eeb81dde7f9f698d940
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    Accepted incoming changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Timmy Du and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
