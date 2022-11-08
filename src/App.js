bd752d1755f5dd590e98e5be59c8a677727fafdc
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    added new project text

--- src/App.js
+++ src/App.js
@@ -8,21 +8,8 @@ function App() {
 <<<<<<< Updated upstream
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-=======
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Shaoyuan Chang and this is my project </p>
->>>>>>> Stashed changes
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
