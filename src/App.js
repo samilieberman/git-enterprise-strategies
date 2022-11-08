affe88369abead4f682f4c4c9ab429b77138e779
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    fixing the conflicts ready to merge

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
