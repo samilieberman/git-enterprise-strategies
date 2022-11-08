b3d61a91f516a0fab85561b92c120070f150329f
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    fixed merge conflict between newText and main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import gatorLogo from './gator.gif';
+import logo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
         <p> My name is Carson Passero and this is my project </p>
       </header>
     </div>
