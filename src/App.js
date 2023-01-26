21ad416abf8c987aa56524d18668d1722561b6a7
kcolindres <92882392+kcolindres@users.noreply.github.com>
2023-01-26 00:33:23 -0500

    Json package changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import gatorLogo from './gator.gif';
-import './App.css';
+import gatorLogo from "./gator.gif";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Sami and this is my project </p>
+        <p>
+          {" "}
+          Hiiiiiii. My name is Krisna Colindres and this is my swe project!{" "}
+        </p>
       </header>
     </div>
   );
