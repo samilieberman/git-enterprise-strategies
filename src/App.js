b9decad4624e38e8606907e80d700854170e500e
michellez3 <michellezou02@gmail.com>
2023-01-30 00:43:06 -0500

    resolves merge conflict between newText branch and main branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import gatorLogo from "./gator.gif";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Michelle and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World! This is our team project.</p>
       </header>
     </div>
   );
