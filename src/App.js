f7c7af593e1d3c67879c2968c37523a75a84294e
WasiChowdhury <wasichowdhury444@yahoo.com>
2023-01-29 22:13:56 -0500

    added new project text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import gatorLogo from './gator.gif';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Wasi Chowdhury and this is my project </p>
+=======
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
+>>>>>>> 242861e (added new project text)
       </header>
     </div>
   );
