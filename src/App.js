4ced11c617d2d43f480a3e307a12e5f28bc26b47
Sam Bricker <sbricker216@gmail.com>
2023-09-01 13:03:39 -0400

    Squashed the commits
    1.0.0
    
    Added extra sentence for commit
    
    Added extra sentence for commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Sami and this is my project </p>
+        <p> My name is Samuel Bricker and this is my project </p>
+        <p>Extra sentence for commit 1</p>
+        <p>Extra sentence for commit 2</p>
       </header>
     </div>
   );
