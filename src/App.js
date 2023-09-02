23c272444c33bee725baee90073abb7a9ca470c5
Sam Bricker <sbricker216@gmail.com>
2023-09-02 15:52:27 -0400

    Resolves conflict

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! This is our team project.
-        </p>	
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Samuel Bricker and this is my project </p>
+        <p>Extra sentence for commit 1</p>
+        <p>Extra sentence for commit 2</p>
       </header>
     </div>
   );
