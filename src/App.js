41d774e97011cac614b9564295cf18ca3e00c97d
Chrystal Williams <cwilliams15@ufl.edu>
2023-09-03 17:56:31 -0400

    Changed gator image and added "Hello class!" text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import gatorLogo from './gator.gif';
+import gatorLogo from './Gator.png';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello class! </p>
         <p> Hello World! </p>
         <p> My name is Chrystal and this is my project </p>
       </header>
