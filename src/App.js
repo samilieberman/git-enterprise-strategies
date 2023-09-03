4e2bc6dc479a0e69d45ee2df29e7123793d2f546
Chrystal Williams <cwilliams15@ufl.edu>
2023-09-03 19:21:43 -0400

    Merged newText and main logo so variables match

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import gatorLogo from './Gator.png';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p> Hello class! </p>
         <p> Hello World! </p>
         <p> My name is Chrystal and this is my project </p>
