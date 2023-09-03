99863fe679bdd9e355bafe53454704392f05f1ef
Ryder-Valiquette <ryder.valiquette@ufl.edu>
2023-09-03 18:52:06 -0400

    Updated App.js
    Updated App.js
    
    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<<<<<<< HEAD
-        <img src={logo} className="App-logo" alt="logo" />
+          <header className="App-header">
+        newText
+        <img src={gatorLogo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
         </p>	
-=======
+
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Ryder and this is my project </p>
->>>>>>> 739950c72b4bb35e51a7b3b808c7bdcfe6e7145a
       </header>
     </div>
   );
