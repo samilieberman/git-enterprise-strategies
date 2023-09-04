25a7f503c1d86af03898344c893e3e871a2021d6
Michael Tu <michaeltu12345@gmail.com>
2023-09-03 20:43:43 -0400

    changed logo to gatorLogo in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={gatorLogo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
         </p>	
