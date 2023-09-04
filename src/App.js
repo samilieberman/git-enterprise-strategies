9bd06d8d5d0cc6826eb2d9559a179496b7d5fe6b
Katja <92826991+kkaroleski2021@users.noreply.github.com>
2023-09-04 18:49:08 -0400

    Revert "STEP 3 COMMITS: Name change in App.js and version update"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import gatorLogo from './gator.gif';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Katja Karoleski and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
