e2779fa4f08eeeb8fe9bcd41308a9d30a6870be0
micielkirsten <micielkirsten@gmail.com>
2023-09-04 08:51:18 +0800

    Fixed main merge conflicts

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! This is our team project.
-        </p>	
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Kirsten and this is my project </p>
       </header>
     </div>
   );
