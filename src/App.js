354c2a70b1c81e01681538313598948bade212f6
volyc <volstadc923@gmail.com>
2024-01-18 14:41:26 -0500

    My Name Update
    1.0.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Sami and this is my project </p>
+        <p> My name is Christopher and this is my project </p>
       </header>
     </div>
   );
