8517ae1e1691790b4d57d2eb518b38ed6b21a1f6
Nat-FL <nrgraddy@gmail.com>
2024-01-23 17:07:18 -0500

    Updated name in App component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Sami and this is my project </p>
+        <p> My name is Natahja Graddy and this is my project </p>
       </header>
     </div>
   );
