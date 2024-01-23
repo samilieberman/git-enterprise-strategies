da6ef4689ef5f30bb3ba91581588ea66d806934f
sethbehar <sethbehar@gmail.com>
2024-01-23 12:12:31 -0500

    resolving conflicts step 7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! This is our team project.
+          Hello World! This is our team project. 
         </p>	
       </header>
     </div>
